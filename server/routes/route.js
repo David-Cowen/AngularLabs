59ded9ab13be5849c9d5b0526bf3c64e844c3470
David Cowen <david.john.cowen@gmail.com>
2021-09-16 22:38:41 +1000

    Strange app breaking error - impossible to fix.

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,23 +1,22 @@
 module.exports = function(req, res) {
     
-
-    var user = {};
-    var u = req.body.email;
-    var p = req.body.password;
+    // var user = {};
+    var u = req.body.formUser.email;
+    var p = req.body.formUser.password;
     let i = userArray.findIndex(user => 
-        ((user.username == u) && (user.password == p)));
+        ((user.email == u) && (user.password == p)));
     if (i == -1) {
         res.send({ "valid": false });
     } else {
         res.send({ "valid": true, user: users[i] });
     }
 
-    let userArray =
-        [
-            {username: "David", birthday: "27/01/1987", age: 34, email: "dave@email.com", password: "123", valid: false},
-            {username: "Mei", birthday: "29/03/1987", age: 34, email: "mei@email.com", password: "123", valid: false},
-            {username: "Isla", birthday: "20/12/2017", age: 3, email: "isla@email.com", password: "123", valid: false}
-        ]
+    // let userArray =
+    //     [
+    //         {username: "David", birthday: "27/01/1987", age: 34, email: "dave@email.com", password: "123", valid: false},
+    //         {username: "Mei", birthday: "29/03/1987", age: 34, email: "mei@email.com", password: "123", valid: false},
+    //         {username: "Isla", birthday: "20/12/2017", age: 3, email: "isla@email.com", password: "123", valid: false}
+    //     ]
 }
 
 
--- server/server.js
+++ server/server.js
@@ -1,18 +1,24 @@
 var express = require('express');
+var cors = require('cors');
 var app = express();
+
+
+app.use(cors());
+
+app.use(express.static(__dirname + '/../dist/week4'));
+
 var http = require('http').Server(app);
-
-app.use(express.static(__dirname + '/api'));
-
 let server = http.listen(3000, function() {
-    console.log("Server listening on port 3001.")
+    console.log("Server listening on port 3000.")
 });
 
-app.get('*', (req, res) => {
-    res.sendFile('C:/Users/Vic/Desktop/Uni Tri 2 2021/Software Frameworks 3813ICT/Assignment/softwareFrameworks/icqu/dist/ICQu/')
-})
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + "/");
+});
 
-app.get('/', function (req, res){
-    res.sendFile(__dirname + '/login')
-})
+// app.get('*', (req, res) => {
+//     res.sendFile('C:/Users/Vic/Desktop/Uni Tri 2 2021/Software Frameworks 3813ICT/Week 5/AngularLabs/dist/week4')
+// })
+
 app.post('/login', require('./routes/route'));
+app.post('/account', require('./routes/accountRoute'))
